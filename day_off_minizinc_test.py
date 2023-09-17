bc6bec8a366cfcbe00d0e021e7e9d7d3bd770c3d
joeyio <107382476+joeyio@users.noreply.github.com>
2023-09-17 19:22:07 -0400

    rewrite using python best practices

--- day_off_minizinc_test.py
+++ day_off_minizinc_test.py
@@ -4,76 +4,67 @@ import numpy as np
 import itertools
 from enum import Enum
 
-# changeable parameters 
-vd = {"SUN", "MON", "TUE", "WED", "THU", "FRI"}
+# loads parameters from a text (.txt or .data) file where each item is on a separate line into a list
+def load_parameters(filename: str):
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
 
-# sample data
-sample_data = pd.DataFrame(data = {'person' : ['bob', 'sally', 'charlie', 'megan', 'alice', 'zach', 'rich', 'john', 'kristen', 'mark'],
-                            'department' : pd.Series(['small class']).repeat(10),
-                            'sub_department' : ['atv', 'art', 'stem', 'culinary', 'art', 'bikes', 'bikes', 'culinary', 'atv', 'stem'],
-                            'first_choice' : ['SUN', 'TUE', 'FRI', 'THU', 'SUN', 'TUE', 'TUE', 'WED', 'FRI', 'TUE'],
-                            'second_choice' : ['MON', 'SUN', 'SUN', 'TUE', 'WED', 'THU', 'SUN', 'FRI', 'SUN', 'WED'],
-                            'third_choice' : ['WED', 'THU', 'MON', 'FRI', 'TUE', 'MON', 'WED', 'THU', 'THU', 'SUN'],
-                            'same_day_as': ['sally', 'bob', 'alice, bob, zach', 'mark, alice' , 'kristen, megan, sally', 'john', 'john, mark', 'alice, rich', 'charlie', 'john'],
-                            # 'same_day_as': ['sally, alice', 'bob, zach', 'alice, bob', 'mark, alice' , 'kristen, megan', 'john, rich', 'john, mark', 'alice, rich', 'charlie, megan', 'john, kristen'],
-                            'strong_pref_ppl' : [1,0,1,1,0,0,1,0,1,1]
-                            }).reset_index(drop=True)
+# load sample data
+def load_sample_data():
+    return pd.read_json('sample_data.json')
 
-print(sample_data)
+# intermediate processing steps for the day off optimizer
+def intermediate_processing(data: pd.DataFrame):
+    # stores a 2d array of day preferences only
+    pref_table = data[['first_choice', 'second_choice', 'third_choice']].to_numpy()
 
-preference_data = sample_data
+    # preferences to have the same day off with others
+    # minizinc does not allow jagged multi-dim arrays. must create dummy values to fill array, up to # columns = max_number
+    same_day_array = data['same_day_as'].str.split(', ', expand=True)
+    max_number = same_day_array.shape[1]
+    
+    # fill empty values with self-references as dummy values
+    for i in range(1,max_number):
+        same_day_array.loc[:,i].fillna(value = data.person, inplace=True)
 
-# intermediate processing steps
-
-# day preferences
-pref_table = preference_data[['first_choice', 'second_choice', 'third_choice']].to_numpy()
-
-# preferences to have the same day off with others
-# minizinc does not allow jagged multi-dim arrays. must create dummy values to fill array, up to # columns = max_number
-
-same_day_array = preference_data['same_day_as'].str.split(', ', expand=True)
-max_number = same_day_array.shape[1]
-# print(max_number)
-for i in range(1,max_number):
-    same_day_array.loc[:,i].fillna(value = preference_data.person, inplace=True)
-#.to_numpy()
-# 
-
-
-#np.array(list(itertools.zip_longest(preference_data['same_day_as'].tolist(), fillvalue='')), dtype=object)
-
-# print(same_day_array)
-
-# people list
-# ppl_list = preference_data['person'].tolist()
-# ppl_list.append('null_val')
-
-# print("people list:")
-# print(ppl_list)
-
+    return pref_table, same_day_array, max_number
 
 # initialize the minizinc model from file
-model = mzn.Model("day_off_python.mzn")
-instance = mzn.Instance(mzn.Solver.lookup("gecode"), model)
+def initialize_model(filename: str):
+    model = mzn.Model(filename)
+    return mzn.Instance(mzn.Solver.lookup("gecode"), model)
 
 # load data into the instance
-instance["DAYS"] = Enum("Days", list(vd))
-#instance["People"] = Enum("People", (preference_data['person']).tolist())
-instance["People"] = preference_data['person'].tolist()
-instance["preferences"] = pref_table
-instance["dept"] = Enum("dept", preference_data['sub_department'].unique().tolist())
-instance["dept_list"] = preference_data['sub_department'].tolist()
-instance["max_same_day_as"] = max_number
-instance["same_day_as_array"] = same_day_array.to_numpy()
-instance["strong_pref_ppl"] = preference_data['strong_pref_ppl'].to_numpy()
+def populate_model(instance: mzn.Instance, data: pd.DataFrame, intermediate_data: tuple, config_params: set):
+    instance["DAYS"] = Enum("Days", list(config_params))
+    instance["People"] = data['person'].tolist()
+    instance["preferences"] = intermediate_data[0]
+    instance["dept"] = Enum("dept", data['sub_department'].unique().tolist())
+    instance["dept_list"] = data['sub_department'].tolist()
+    instance["max_same_day_as"] = intermediate_data[2]
+    instance["same_day_as_array"] = intermediate_data[1].to_numpy()
+    instance["strong_pref_ppl"] = data['strong_pref_ppl'].to_numpy()
 
 # show results
-result = instance.solve()
-print(result)
+def show_results(instance: mzn.Instance, data: pd.DataFrame):
+    result = instance.solve()
+    print(result)
+    print(result.solution.assignment)
+    result_table = pd.DataFrame({'name' : data.person, 
+                                'assignment' : result.solution.assignment})
+    print(result_table)
 
-print(result.solution.assignment)
+def main():
+    vd = set(load_parameters('valid_days.data'))
+    preference_data = load_sample_data()
+    intermediate_data = intermediate_processing(preference_data)
+    print(preference_data)
 
-result_table = pd.DataFrame({'name' : preference_data.person, 
-                             'assignment' : result.solution.assignment})
+    instance = initialize_model('day_off_python.mzn')
+    populate_model(instance, preference_data, intermediate_data, vd)
+    show_results(instance, preference_data)
 
-print(result_table)
+if __name__ == '__main__':
+    main()
+
